95d21164f0bf0dfe55139cb3de3f66bc52da5d3a
MitchellyM17 <Mitchellymasengi@gmail.com>
2024-10-02 14:34:51 +0800

    Add files via upload

---
+++ FrmPngguna.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateData
+{
+    public partial class FrmPngguna : Form
+    {
+        public FrmPngguna()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
